75925fa2cec549b9b2bda28b892737194c75d577
Ray <rguo1010@uw.edu>
2024-06-30 23:37:01 -0700

    General style formatting

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import './stylesheets/colorTheme.css';
+import './stylesheets/mainPage.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Test
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="page">
+      <div className = "title">
+        Ray Guo
+      </div>
     </div>
   );
 }
